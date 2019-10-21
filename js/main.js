cd42fbb309e3ab0f364ad4892f3ca4d7d7d1e3f7
yurikoguch <yurikoguch@gmail.com>
2019-10-22 00:52:22 +0300

    Added date and styles

--- js/main.js
+++ js/main.js
@@ -13,16 +13,27 @@ function newElement() {
     document.getElementById("myInput").value = "";
 
 
+    // Add date
+
+    const today = new Date();
+    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getHours() + '-' + today.getMinutes();
+
+    let dateContainer = document.createElement('td');
+    let dateTXT = document.createTextNode(date);
+    dateContainer.appendChild(dateTXT);
+    tr.appendChild(dateContainer);
+    dateContainer.className = "date";
+
     // Add delete button
 
-    var deleteBTN = document.createElement("button");
-    var txt = document.createTextNode("\u00D7");
+    var deleteBTN = document.createElement("td");
+    var txt = document.createTextNode("Close");
     deleteBTN.appendChild(txt);
     deleteBTN.className = "close";
     tr.appendChild(deleteBTN);
 
     for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
+        close[i].onclick = function () {
             let row = this.parentElement;
             row.remove();
         }
@@ -30,18 +41,22 @@ function newElement() {
 
     // Add edit Button
 
-    var editBTN = document.createElement("button");
+    var editBTN = document.createElement("td");
     var editTXT = document.createTextNode("Edit");
     editBTN.appendChild(editTXT);
     editBTN.className = "edit";
     tr.appendChild(editBTN);
 
     for (i = 0; i < close.length; i++) {
-        edit[i].onclick = function() {
-          let editRow = this.parentElement;
+        edit[i].onclick = function () {
+            let editRow = this.parentElement;
+
         }
     }
+
 }
+
+
 
 
 // Click on a close button to delete the current list item
